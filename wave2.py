9b40aaf5536b9702790e7beca40fcf563f379757
shawa84326 <148395690+shawa84326@users.noreply.github.com>
2024-02-22 17:47:15 -0800

    Create wave2.py

---
+++ wave2.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+# forward_kinematics.py: Move the fetch arm to a desired pose using forward kinematics
+import rospy
+from moveit_msgs.msg import MoveItErrorCodes
+from moveit_python import MoveGroupInterface, PlanningSceneInterface
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
+
+# Note: fetch_moveit_config move_group.launch must be running
+# Safety!: Do NOT run this script near people or objects.
+# Safety!: There is NO perception.
+#          The ONLY objects the collision detection software is aware
+#          of are itself & the floor.
+if __name__ == '__main__':
+    rospy.init_node("forward_kinematics_example")
+
+    # Create move group interface for a fetch robot
+    move_group = MoveGroupInterface("arm_with_torso", "base_link")
+
+    # Define ground plane
+    planning_scene = PlanningSceneInterface("base_link")
+    planning_scene.removeCollisionObject("my_front_ground")
+    planning_scene.removeCollisionObject("my_back_ground")
+    planning_scene.removeCollisionObject("my_right_ground")
+    planning_scene.removeCollisionObject("my_left_ground")
+    planning_scene.addCube("my_front_ground", 2, 1.1, 0.0, -1.0)
+    planning_scene.addCube("my_back_ground", 2, -1.2, 0.0, -1.0)
+    planning_scene.addCube("my_left_ground", 2, 0.0, 1.2, -1.0)
+    planning_scene.addCube("my_right_ground", 2, 0.0, -1.2, -1.0)
+
+    # This is the wrist link not the gripper itself
+    gripper_frame = 'wrist_roll_link'
+
+    # Define your desired pose (modify these values accordingly)
+    desired_pose = Pose(Point(0.1, 0.5, 1.5), Quaternion(0.0, 0.0, 0.0, 1.0))
+
+    # Construct a "pose_stamped" message as required by moveToPose
+    gripper_pose_stamped = PoseStamped()
+    gripper_pose_stamped.header.frame_id = 'base_link'
+    gripper_pose_stamped.header.stamp = rospy.Time.now()
+    gripper_pose_stamped.pose = desired_pose
+
+    # Move gripper frame to the desired pose
+    move_group.moveToPose(gripper_pose_stamped, gripper_frame)
+    result = move_group.get_move_action().get_result()
+
+    if result:
+        # Checking the MoveItErrorCode
+        if result.error_code.val == MoveItErrorCodes.SUCCESS:
+            rospy.loginfo("Arm moved to the desired pose!")
+        else:
+            # If you get to this point please search for:
+            # moveit_msgs/MoveItErrorCodes.msg
+            rospy.logerr("Arm goal in state: %s", move_group.get_move_action().get_state())
+    else:
+        rospy.logerr("MoveIt! failure no result returned.")
+
+    # This stops all arm movement goals
+    # It should be called when a program is exiting so movement stops
+    move_group.get_move_action().cancel_all_goals()
